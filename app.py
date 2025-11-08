497685dcd6a5e1a6383af49e7d9c460076714f83
Tristan Martinez <tristandmartinez99@gmail.com>
2025-11-07 22:52:09 -0700

    Added db_create route

--- app.py
+++ app.py
@@ -13,3 +13,23 @@ def connect():
     conn = psycopg2.connect('postgresql://tmar_postgres_user:CB9YlJtgM8sN2OUiaWldcOAAk5si3oC7@dpg-d47d0sq4d50c73835go0-a/tmar_postgres')
     conn.close()
     return 'Database Connection Successful'
+
+@app.route('/db_create')
+def create_db():
+    #use with block so no need to close connection each time
+    with psycopg2.connect('postgresql://tmar_postgres_user:CB9YlJtgM8sN2OUiaWldcOAAk5si3oC7@dpg-d47d0sq4d50c73835go0-a/tmar_postgres') as conn:
+        cursor = conn.connect()
+        try:
+            cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Basketball(
+                First varchar(255),
+                Last varchar(255),
+                City varchar(255),
+                Name varchar(255),
+                Number int
+                );
+                ''')
+            conn.commit()
+            return "Basketball created successfully"
+        except Exception as e:
+            return f"Error {e} occurred"
