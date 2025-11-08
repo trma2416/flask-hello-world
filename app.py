8ac64c2e731a3dd60b4a9c4a11173bf327db95de
Tristan Martinez <tristandmartinez99@gmail.com>
2025-11-07 22:16:12 -0700

    Added route for postgres database

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World from Tristan Martinez in 3308!'
+
+
+@app.route('/db_test')
+def connect():
+    with psycopg2.connect('postgresql://tmar_postgres_user:CB9YlJtgM8sN2OUiaWldcOAAk5si3oC7@dpg-d47d0sq4d50c73835go0-a/tmar_postgres') as conn:
+        return 'Database Connection Successful'
