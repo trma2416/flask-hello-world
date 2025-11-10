2998cfbb41a69f8bcd1299d649b2285f2341122a
Tristan Martinez <tristandmartinez99@gmail.com>
2025-11-10 11:42:30 -0700

    Added all neccessary routes

--- app.py
+++ app.py
@@ -40,6 +40,27 @@ def create_db():
         return f"Error {e} occurred"
     finally:
         conn.close()
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect('postgresql://tmar_postgres_user:CB9YlJtgM8sN2OUiaWldcOAAk5si3oC7@dpg-d47d0sq4d50c73835go0-a/tmar_postgres')
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''
+                       INSERT INTO Basketball (First, Last, City, Name, Number)
+                       VALUES
+                       ('Jason', 'Tatum', 'Boston', 'Celtics', 0),
+                       ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+                       ('Nikola', 'Jokic', 'Denver' 'Nuggets', 15),
+                       ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+                       
+                       ''')
+        conn.commit()
+        return "Basketball Table successfully populated"
+    except psycopg2.Error:
+        return "Error inserting data"
+    finally:
+        conn.close()
         
 @app.route('/db_select')
 def select():
@@ -57,3 +78,16 @@ def select():
         response_str += "</tr>"
     response_str += "</table>"
     return response_str
+
+
+@app.route('/db_drop')
+def drop():
+    conn = psycopg2.connect('postgresql://tmar_postgres_user:CB9YlJtgM8sN2OUiaWldcOAAk5si3oC7@dpg-d47d0sq4d50c73835go0-a/tmar_postgres')
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''DROP TABLE Basketball;
+                       ''')
+        conn.commit()
+        return "Basketball Table Successfully Dropped"
+    finally:
+        conn.close()
