e54d72a87d5bd3c7550b5a44fb53e2be3db59864
Tristan Martinez <tristandmartinez99@gmail.com>
2025-11-10 09:30:30 -0700

    Added db_select route

--- app.py
+++ app.py
@@ -35,3 +35,20 @@ def create_db():
         return f"Error {e} occurred"
     finally:
         conn.close()
+        
+@app.route('/db_select')
+def select():
+    conn = psycopg2.connect('postgresql://tmar_postgres_user:CB9YlJtgM8sN2OUiaWldcOAAk5si3oC7@dpg-d47d0sq4d50c73835go0-a/tmar_postgres')
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * FROM Basketball''')
+    records = cursor.fetchall()
+    conn.close()
+    response_str = ""
+    response_str += "<table>"
+    for player in records:
+        response_str += "<tr>"
+        for info in player:
+            response_str += f"<td>{info}<td>"
+        response_str += "</tr>"
+    response_str += "</table>"
+    return response_str
